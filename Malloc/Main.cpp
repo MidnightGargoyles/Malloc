d257191ba2301606b34986814d671a1027334552
Ryxali <ryxali@gmail.com>
2014-02-15 14:58:24 +0100

    Added SFML

--- Malloc/Main.cpp
+++ Malloc/Main.cpp
@@ -1,4 +1,22 @@
-
+#include "SFML\Window.hpp"
+#include "SFML\System.hpp"
+#include "SFML\Graphics.hpp"
 int main() {
+	sf::RenderWindow win(sf::VideoMode(800, 600), "Malloc");
+	sf::View view(sf::FloatRect(0, 0, 800, 600));
+	sf::Event evt;
+	while(win.isOpen()) {
+		while(win.pollEvent(evt)) {
+			switch(evt.type) {
+			case sf::Event::Closed:
+				win.close();
+				break;
+			default:
+				break;
+			}
+		}
+		win.clear(sf::Color::Black);
+		win.display();
+	}
 	return 0;
 }
